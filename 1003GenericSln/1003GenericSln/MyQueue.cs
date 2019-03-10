2cd84072da1a995a08966dd5e2dbb0394987546b
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-03-10 21:22:44 +0200

    Add files via upload

---
+++ 1003GenericSln/1003GenericSln/MyQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1003GenericSln
+{
+    class MyQueue<T> : IEnumerable<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Enqueue(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            T item = items[0];
+            items.RemoveAt(0);
+            return item;
+        }
+
+        public override string ToString()
+        {
+            string result = $"My Queue {items.Count}";
+            foreach(T t in items)
+            {
+                result += t + "\n";
+            }
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+        }
+
+        
+    }
+}
---
+++ 1003GenericSln/1003GenericSln/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1003GenericSln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create queue for int
+            MyQueue<int> numbers = new MyQueue<int>();
+            numbers.Enqueue(3);
+            numbers.Enqueue(5);
+            numbers.Enqueue(1);
+            numbers.Enqueue(-2);
+            numbers.Enqueue(30);
+
+            Console.WriteLine("For each:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Dequeue item returned: {numbers.Dequeue()}");
+            Console.WriteLine();
+
+            Console.WriteLine("For each:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Item at index 3 : {numbers[3]}");
+
+            // Create queue for float
+            Console.WriteLine();
+            Console.WriteLine("Now float queue:");
+            MyQueue<float> numbersFloat = new MyQueue<float>();
+            numbersFloat.Enqueue(3.7f);
+            numbersFloat.Enqueue(2.4f);
+            Console.WriteLine("For each:");
+            foreach (float num in numbersFloat)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
